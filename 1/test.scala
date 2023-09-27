4e11b8fba1bdfce02e7e412c47daeec9b4fdeb0b
EC2 Default User <ec2-user@ip-172-26-11-114.eu-west-2.compute.internal>
2023-09-27 07:37:42 +0000

    Update test.scala at 2023-09-27 07:37:42.769107 through application.

--- 1/test.scala
+++ 1/test.scala
@@ -1,5 +1,10 @@
+import java.time._
 object test {
     def main(args: Array[String]) = {
         println("Hello, world")
+        for( a <- 1 to 10){
+            println( "Value of a: " + a );
+            Thread.sleep(20000L)
+        }
     }
 }
